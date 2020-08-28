84c7c5a61f8687047bfb7aaf520787aa2a75a031
Matt <mpickle86@gmail.com>
2020-08-28 18:20:26 -0500

    Add procfile for heroku and run build

---
+++ client/build/precache-manifest.98161d3473522411a9e14c7c74084514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a8a6a7f6b4f11cf4759384aefc7604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4b5c8ecc35d13f0c8b0",
+    "url": "/static/css/main.6ebefae8.chunk.css"
+  },
+  {
+    "revision": "ee7dda6a222175d585fa",
+    "url": "/static/js/2.7c85a347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c85a347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b5c8ecc35d13f0c8b0",
+    "url": "/static/js/main.59025efe.chunk.js"
+  },
+  {
+    "revision": "5594ddec7b476653afe6",
+    "url": "/static/js/runtime-main.2ea40438.js"
+  },
+  {
+    "revision": "e3fb4b61596514bdf9d02b6e20f9a4b1",
+    "url": "/static/media/background.e3fb4b61.jpg"
+  }
+]);
